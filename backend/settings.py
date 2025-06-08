17ceecbaee120c7b22570b2e503ff0e95e9b48e1
alfredo <halitoxd13@gmail.com>
2025-06-07 18:55:02 -0600

    tu mensaje

--- backend/settings.py
+++ backend/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework',            # <<< agregado
+    'rest_framework.authtoken',  # <<< agregado para TokenAuthentication
     'api',
+    'django_extensions',
+
 ]
 
 MIDDLEWARE = [
