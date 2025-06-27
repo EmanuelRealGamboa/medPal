1c4be045e53d43da06101df200712d598f885785
Josue Ramses <josueramsesrojasvallejo@gmail.com>
2025-06-27 13:42:28 -0600

    Add reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 # accounts/urls.py
 from django.urls import path
 from .views import SignupView, VerifyCodeView, SigninView, LogoutView
+from .views import RequestPasswordResetView, ResetPasswordView
 
 #Rutas para los Edpoints
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('verify/', VerifyCodeView.as_view()),
     path('signin/', SigninView.as_view()),
     path('logout/', LogoutView.as_view()),
-    
+    path('request-password-reset/', RequestPasswordResetView.as_view()),
+    path('reset-password/', ResetPasswordView.as_view()),
 ]
