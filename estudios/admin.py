42d69f2c4f14aa7743f9c9899c2e7231d671f5e2
jonathan <yahirsalanueva@gmail.com>
2025-07-08 17:08:58 -0600

    feat: modelos y admin para estudios médicos (gabinete, laboratorio, funcionales)

--- estudios/admin.py
+++ estudios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import EstudioGabinete, EstudioLaboratorio, EstudioFuncional
 
 # Register your models here.
+
+
+admin.site.register(EstudioGabinete)
+admin.site.register(EstudioLaboratorio)
+admin.site.register(EstudioFuncional)
